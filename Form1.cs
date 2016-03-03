19f1f459884472ede93cdeeb7cac50c93aeaef67
mauriciolfsilva <mauriciolfsilva@yahoo.com.br>
2016-03-03 11:13:56 -0300

    Lista Encadeada
    Lista Encadeada

--- Form1.cs
+++ Form1.cs
@@ -13,21 +13,80 @@ namespace ListaEncadeada
     public partial class Form1 : Form
     {
         Lista lista = new Lista();
-        Element a = new Element(0);
-        Element b = new Element(1);
-        Element c = new Element(2);
-        Element d = new Element(3);
-        Element e = new Element(7);
 
         public Form1()
         {
-            lista.Add(a);
-            lista.Add(b);
-            lista.Add(c);
-            lista.Add(d);
-            lista.Print();
-            lista.AddAt(e,2);
-            lista.Print();
-        }
+			InitializeComponent();
+        }	
+
+		private void Add(object sender, EventArgs e)
+		{
+			int v = lista.Count();
+			Element a = new Element(v);
+
+			lista.Add(a);
+			Console.WriteLine("Add Element with value " + v);
+			lista.Print();
+		}
+
+		private void AddAt(object sender, EventArgs e)
+		{
+			Random r = new Random();
+			int p = r.Next(1,lista.Count());
+			int v = lista.Count();
+			Element a = new Element(v);
+
+			lista.AddAt(a, p);
+			Console.WriteLine("Add Element with value " + v + " at position " + p);
+			lista.Print();
+		}
+
+		private void DelAt(object sender, EventArgs e)
+		{
+			Random r = new Random();
+			int p = r.Next(1, lista.Count());
+
+			lista.DelAt(p);
+			Console.WriteLine("Delete Element at position " + p);
+			lista.Print();
+		}
+
+		private void ChangePos(object sender, EventArgs e)
+		{
+			if (lista.Count() >= 2)
+			{
+				Random r = new Random();
+				int p = r.Next(1, lista.Count());
+				int p2 = r.Next(1, lista.Count());
+
+				while (p == p2)
+				{
+					r.Next(1, lista.Count());
+				}
+
+				lista.ChangePos(p,p2);
+				Console.WriteLine("Change the position of the elements in the positions " + p + " and " + p2);
+				lista.Print();
+			}
+
+			else
+			{
+				Console.WriteLine("There is only one element in this List, you can't do this");
+			}
+
+		}
+
+		private void AddAfter(object sender, EventArgs e)
+		{
+			int v = lista.Count();
+			Element a = new Element(v);
+			Random r = new Random();
+			int v2 = r.Next(1, lista.Count());
+
+			lista.AddAfter(a, v2);
+			Console.WriteLine("Add Element whith value " + v + " after the Element whith value " + v2);
+			lista.Print();
+
+		}
     }
 }
--- Lista.cs
+++ Lista.cs
@@ -36,18 +36,62 @@ namespace ListaEncadeada
 
         public void AddAt(Element a, int p)
         {
-            int i = 1;
-            Element act = root;
-            while (i < p - 1)
-            {
-                act = act.Next;
-                i++;
-            }
+            Element act = FindAt(p - 1);
 
             a.Next = act.Next;
             act.Next = a;
-
         }
+
+		private Element FindAt(int p)
+		{
+			int i = 1;
+			Element act = root;
+			while (i < p)
+			{
+				act = act.Next;
+				i++;
+			}
+
+			return act;
+		}
+
+        public void AddAfter(Element a, int v)
+        {
+            Element act = root;
+            while(act.Next != null && act.Me != v)
+            {
+                act = act.Next;
+            }
+
+            if(act.Me == v)
+			{
+				a.Next = act.Next;
+				act.Next = a;
+            }
+
+			else
+			{
+				Console.WriteLine("There isn't an Element with value " + v);
+			}
+        }
+
+		public void ChangePos(int p1, int p2)
+		{
+			Element e1 = FindAt(p1);
+			Element e2 = FindAt(p2);
+			int temp = e1.Me;
+
+			e1.Me = e2.Me;
+			e2.Me = temp;
+		}
+
+		public void DelAt(int p)
+		{
+			Element e1 = FindAt(p - 1);
+			Element e2 = FindAt(p);
+
+			e1.Next = e2.Next;
+		}
 
         public void Print()
         {
@@ -59,5 +103,19 @@ namespace ListaEncadeada
                 act = act.Next;
             }
         }
+
+		public int Count()
+		{
+			Element act = root;
+			int i = 1;
+
+			while (act != null)
+			{
+				i++;
+				act = act.Next;
+			}
+
+			return i;
+		}
     }
 }
